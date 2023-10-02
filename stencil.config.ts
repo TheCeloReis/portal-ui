1cd3c1f66b3c76523375b04dfd770e4b19f72a7f
Celo Reis <m.reis@portaltelemedicina.com.br>
2023-10-02 09:08:19 -0300

    Initial commit

---
+++ stencil.config.ts
@@ -0,0 +1,25 @@
+import { Config } from '@stencil/core';
+import { vueOutputTarget } from '@stencil/vue-output-target';
+
+export const config: Config = {
+  namespace: 'portal-ui',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+  testing: {
+    browserHeadless: 'new',
+  },
+};
